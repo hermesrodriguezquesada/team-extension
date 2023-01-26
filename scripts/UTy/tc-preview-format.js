4b64a08f07e08d98b9aeab7ccf6800b739cd19ce
hermesrodriguezquesada <75341312+hermesrodriguezquesada@users.noreply.github.com>
2023-01-26 07:01:29 +0000

    Update files popup.html and scripts/UTy/tc-preview-format.js

--- scripts/UTy/tc-preview-format.js
+++ scripts/UTy/tc-preview-format.js
@@ -2,8 +2,37 @@ chrome.runtime.onMessage.addListener(
     function(msg, sender, sendResponse) {
       if(msg.text == "Activar script tc-preview-format") {
         // Ensure the preview dialog is displayed before running the script
-        const info = document.getElementsByClassName('details')
-        info[0].style.setProperty('white-space','break-spaces','important')
+        const details = document.getElementsByClassName('details')[0];
+        details.style.setProperty('white-space', 'pre-wrap', 'important');
+
+        function extractInfo(html) {
+          var result = {};
+          // Extract Action Details
+          var actionDetails = html.match(/----\sACTION\sDETAILS\s----([\s\S]*?)(?=----\sENVIRONMENT\s----)/);
+          result.actionDetails = actionDetails[1].trim().replace(/:\n/g, ":\n\n")
+
+          // Extract Environment
+          var environment = html.match(/----\sENVIRONMENT\s----([\s\S]*?)(?=----\sAPPLAUSE\sPROPERTIES\s----)/);
+          result.environment = environment[1].trim().replace(/:\n/g, ":\n\n")
+
+          // Extract Applause Properties
+          var applauseProperties = /----\sAPPLAUSE\sPROPERTIES\s----([\s\S]*?)----\sAPPLAUSE\sATTACHMENT\(S\)\s----/.exec(html);
+          result.applauseProperties = applauseProperties[0].trim();
+
+        // Extract Applause Attachments
+        var attachments = html.match(/(https:\/\/[^\s]+)/g);
+        result.attachments = attachments.join(" ");
+
+        //Make links
+        result.attachments = result.attachments.replace(/(https:\/\/[^\s]+)/g, function(link){
+          return "<a href='" + link + "' style='width: 500px;'>" + link + "</a>";
+        });
+          return result;
+        }
+
+        var html = details.innerHTML;
+        var info = extractInfo(html);
+        details.innerHTML = info.actionDetails + info.environment + info.applauseProperties + info.attachments;
       }
     }
 )
